00a00633259c4e68cafec4c4e79975dbeceec1e8
Umesh <umeshboda2024.katargam@gmail.com>
2026-01-30 11:31:31 +0530

    add card & update

--- app.js
+++ app.js
@@ -16,9 +16,21 @@ app.get("/", (req, res) => {
 
 app.get("/CreateData", (req, res) => {
   const data = req.query;
-  setdata.push(data);
+  data.id=setdata.length
+  if (data.id) {
+    const updataid=setdata.findupdateid(item => item.id == data.id)
+    setdata[updataid] =data
+  }
+  else{
+   setdata.push(data);
+  }
+  
+
   fs.writeFileSync("json/data.json", JSON.stringify(setdata));
   res.redirect("/");
+});
+app.get("/add", (req, res) => {
+  res.render("app.ejs", {setdata });
 });
 
 app.get("/Editdata/:id", (req, res) => {
@@ -26,7 +38,20 @@ app.get("/Editdata/:id", (req, res) => {
   const Editdata = setdata[editid];
   res.render("product.ejs", { Editdata, setdata });
 });
+app.get("/Deletedata/:id", (req, res) => {
+  const Deletedata = req.params.id;
+  setdata.splice(Deletedata, 1);
 
-app.listen(3000, () => {
+  fs.writeFileSync("json/data.json", JSON.stringify(setdata));
+  res.redirect("/");
+});
+app.get("/Updatedata/:id" ,(req,res) =>{
+  const Updatedata= setdata.find(item => item.id == req.params.id)
+   res.render("app.ejs", { Updatedata });
+
+})
+
+
+app.listen(3002, () => {
   console.log("Server running on 3000");
 });
